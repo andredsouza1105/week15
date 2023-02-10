3f65e72d728b874a4cd398a6a193398d233da727
andredsouza1105 <andredsouza1105@gmail.com>
2023-02-10 10:11:11 +0400

    abc

---
+++ products.js
@@ -0,0 +1,47 @@
+let products= [ 
+    {
+        id: 101,
+        title: "Cat Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable<\em>" + " organic goodness for your cat.",
+        price: 2000,
+        image: "images/catfood.jpg",
+        availableInventory:10,
+        rating:3
+    },
+    {
+        id: 102,
+        title: "Dog Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable<\em>" + " organic goodness for your dog.",
+        price: 2000,
+        image: "images/petfood.JFIF",
+        availableInventory:10,
+        rating:3
+    },
+    {
+        id: 103,
+        title: "Bird Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable<\em>" + " organic goodness for your bird.",
+        price: 2000,
+        image: "images/birdfood.jpg",
+        availableInventory:10,
+        rating:3
+    },
+    {
+        id: 104,
+        title: "Rabbit Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable<\em>" + " organic goodness for your Rabbit.",
+        price: 2000,
+        image: "images/rabbitfood.jpg",
+        availableInventory:10,
+        rating:3
+    },
+    {
+        id: 105,
+        title: "Turtle Food, 25lb bag",
+        description: "A 25 pound bag of <em>irresistable<\em>" + " organic goodness for your turtle.",
+        price: 1000,
+        image: "images/turtlefood.jpg",
+        availableInventory:10,
+        rating:3
+    },
+]
